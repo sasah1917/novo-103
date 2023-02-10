8d5441e04a3570567d97a797d62f56f45541ce69
sasah1917 <92746412+sasah1917@users.noreply.github.com>
2023-02-10 13:40:51 -0300

    Add files via upload

---
+++ file_system_events_tracker.py
@@ -0,0 +1,31 @@
+import sys
+import time
+import random
+import os
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class FileEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"Ola, {event.src_path} foi criado")
+    def on_deleted(self, event):
+        print(f"opa! Alguem excluiu {event.src_path}!")
+    def on_modified(self, event):
+        print(f"alguem modificou {event.src_path}")
+    def on_moved(self, event):
+        print(f"alguem moveu {event.src_path}")
+
+event_handler = FileEventHandler() 
+observer = Observer() 
+observer.schedule(event_handler, from_dir, recursive=True) 
+observer.start() 
+try:
+    while True:
+        time.sleep(2)
+        print("executando...")
+except KeyboardInterrupt:
+    print("Interrompido!")
+    observer.stop()
+
+
